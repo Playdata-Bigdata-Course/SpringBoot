b14dc7e5a6cfdf8b1363191fcd1d4458ced2ae83
seongho726 <sungho121@naver.com>
2021-02-02 17:29:17 +0900

    21.02.02

---
+++ step06_bootRest/src/main/java/io/playdata/controller/Controller.java
@@ -0,0 +1,58 @@
+package io.playdata.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.playdata.model.domain.Board;
+
+@RestController
+public class Controller {
+	public Controller() {
+		System.out.println("------- Controller 생성자");
+	}
+	
+	//GET - http://localhost:80/hello
+	@GetMapping("hello")
+	public String m1() {
+		return "정신똑바로 차려라";
+	}
+	//GET - http://localhost:80/hello2?id=tester
+	@GetMapping("hello2")
+	public String m2(String id) {
+		return "정신이 차려져?" + id;
+	}
+	
+	/* RestController 기반의 GetMapping 사용시
+	 * DTO 클래스들은 멤버 변수명과 데이터값들이 JSON 형식으로 응답
+	 */
+	//GET - http://localhost:80/board
+	@GetMapping("board")
+	public Board m3() {
+		return new Board(1, "spring boot", "권희성", "으갸갸갹");
+	}
+	
+	//GET - http://localhost:80/boardall
+	@GetMapping("boardall")
+	public List<Board> m4(){
+		List<Board> all = new ArrayList<>();
+		for(int i=0; i < 10; i++) {
+			all.add(new Board(i,
+							"spring boot" +i,
+							"유재석"	+ i,
+							"spring app을 쉽게 개발 가능하게 하는 도구"));
+		}
+		return all;
+	}
+	
+	//REST API + URI Template
+	//GET - http://localhost:80/boarduri/{?}
+	@GetMapping("boarduri/{id}")
+	public String m5(@PathVariable String id) {
+		return "으게게게겍" + id;
+	}
+	
+}
---
+++ step07_bootSpringDataJPA/src/main/java/io/playdata/controller/BoardController.java
@@ -0,0 +1,69 @@
+package io.playdata.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.playdata.dao.BoardRepository;
+import io.playdata.model.domain.Board;
+
+@RestController
+public class BoardController {
+	
+	@Autowired
+	private BoardRepository dao;
+	
+	//http://127.0.0.1/board?i=2
+	//http://127.0.0.1/board?i=3
+	//select count(*) from board;
+	@PostMapping("board")
+	public String m1(Long i) {
+		Board save =  new Board(i, "spring boot", "권희성", "으갸갸갹");
+		dao.save(save);
+		return "게시판 저장 성공";
+	}
+	
+	//http://ip:port/boarddata?id=1
+	@GetMapping("boarddata")
+	public Board m2(Long id) {
+		System.out.println("---------------" + id);
+		Optional<Board> cn = dao.findById(id);
+		
+		System.out.println("---------------" + cn.get());
+		return cn.get();
+	}
+	//json 포멧으로 응답
+	//http://127.0.0.1/boardall
+	@GetMapping("boardall")
+	public Iterable<Board> m3() {
+		Iterable<Board> all = dao.findAll();
+		return all;
+	}
+	//http://127.0.0.1/board-title
+	@GetMapping("board-title")
+	public List<Board> m4(){
+		List<Board> all =dao.findBoardByTitle("spring boot");
+		System.out.println(all);
+		return all;
+	}
+	
+	//http://127.0.0.1/board-titlecontain
+	@GetMapping("board-titlecontain")
+	public List<Board> m5(){
+		List<Board> all =dao.findBoardByTitleContaining("boot");
+		System.out.println(all);
+		return all;
+	}
+}
+/* 에러가 난 상황
+ * 데이터가 없음
+ * create -> http://127.0.0.1/board -> 메소드 추가 개발 및 저장 : 실행환경 초기화 (create)되는 상황
+ * 따라서  none으로 수정
+ * table 생성 존재여부 확인
+ * board 데이터 저장 - http://127.0.0.1/board
+ * 검색 - http://127.0.0.1/boarddata?id=1
+ */
